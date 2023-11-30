98ae4f5f07aa2d132f38c6bfef40a297bff29070
ShiftyERS <connormdooley@gmail.com>
2023-11-29 22:22:48 -0500

    WeaponData

--- ShootGame/Assets/Scripts/WeaponData.cs
+++ ShootGame/Assets/Scripts/WeaponData.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "NewWeapon", menuName = "Weapons/New Weapon")]
 public class WeaponData : ScriptableObject
 {
     public int damage;
     public int currentAmmo;
     public int maxAmmo;
     public int fireRate;
-    public bool isReloading;
+
     public float reloadTime;
+
+    public bool isReloading;
+    public bool isAutomatic;
 }
