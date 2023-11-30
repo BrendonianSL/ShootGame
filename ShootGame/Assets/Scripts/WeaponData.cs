dfd051f47f99fa0ba6f4f926b9512fbbf7c927bd
ShiftyERS <connormdooley@gmail.com>
2023-11-29 23:02:31 -0500

    TargetInterface

--- ShootGame/Assets/Scripts/WeaponData.cs
+++ ShootGame/Assets/Scripts/WeaponData.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 [CreateAssetMenu(fileName = "NewWeapon", menuName = "Weapons/New Weapon")]
 public class WeaponData : ScriptableObject
 {
